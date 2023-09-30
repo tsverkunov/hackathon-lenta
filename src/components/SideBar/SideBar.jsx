839124c6fc70b95e3a0713be68be34c18d1df993
Daniil Borovov <superskillbro@gmail.com>
2023-09-30 19:44:56 +0600

    add collapse animations

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,14 +1,29 @@
 import style from './SideBar.module.css'
 
 export default function SideBar() {
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log('Открыть прогноз')
+  }
+
+  function handleCollapse(e) {
+    e.preventDefault()
+    const sidebar = document.querySelector(`.${style.sidebar}`)
+    sidebar.classList.toggle(style.collapsed)
+  }
+
   return (
     <div className={style.sidebar}>
       <div className={style.collapse}>
-        <button className={style.collapseButton} />
+        <button className={style.collapseButton} onClick={handleCollapse} />
       </div>
-      <div className={style.filters}></div>
+      <div className={style.filters}>
+        <form action='submit'></form>
+      </div>
       <div className={style.footer}>
-        <button className={style.filterButton}>Открыть прогноз</button>
+        <button className={style.filterButton}
+          onClick={handleSubmit}
+        >Открыть прогноз</button>
       </div>
     </div>
   )
