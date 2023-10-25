78187111b65a5dbd677251e79aea5abb6b49f0ea
ITsverkunov <tsverkunovivan@gmail.com>
2023-10-26 01:05:37 +0300

    fix: Deploy to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/hackathon-lenta",
   plugins: [react()],
   server: {
     host: true,
     port: 3000,
   },
-  build: { 
+  build: {
     chunkSizeWarningLimit: 1000
   },
 })
